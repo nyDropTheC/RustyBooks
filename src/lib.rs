7a214c0520d850edad61a36e95a8bca4044497d2
Nik <125159296+nyDropTheC@users.noreply.github.com>
2023-06-21 23:23:13 +0300

    aaa

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
 use worker::*;
-use reqwest::Client;
+
+use reqwest::{Client, Result};
 use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="lowercase")]
+pub enum ChatCompletionRole {
+    System,
+    User,
+    Assistant
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChatMessage {
+    role: ChatCompletionRole,
+    content: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChatRequest {
+    model: String,
+    messages: Vec<ChatMessage>,
+    temperature: f32
+}
 
 #[derive(Serialize, Deserialize)]
 struct BookData {
@@ -8,38 +30,68 @@ struct BookData {
     name: String
 }
 
-#[derive(Serialize, Deserialize)]
-struct ChatMessage {
-    role: String,
-    message: String
+enum RequestType {
+    Summarize,
+    Similar
 }
 
-#[derive(Serialize, Deserialize)]
-struct ChatCompletionRequest {
-    model: String,
-    messages: Vec<ChatMessage>
-}
+async fn openai_call(api_key: String, req_type: RequestType, _data: &BookData) {
+    let cl = Client::new();
 
-async fn openai_call(api_key: String, data: BookData) {
-    let client = Client::new();
+    let messages = match req_type {
+        RequestType::Summarize => {
+            vec![
+                ChatMessage {
+                    role: ChatCompletionRole::User,
+                    content: "Hi!".to_string()
+                }
+            ]
+        },
+        RequestType::Similar => {
+            vec![
+                ChatMessage {
+                    role: ChatCompletionRole::User,
+                    content: "Hi!".to_string()
+                }
+            ]
+        }
+    };
 
-    let _ = client
-        .post("https://api.openai.com/v1/chat/completions")
+    let request = ChatRequest {
+        model: "gpt-3.5-turbo".to_string(),
+        temperature: 1.0f32,
+        messages: messages
+    };
+
+    console_log!("{}", serde_json::to_string(&request).unwrap());
+
+    let result = cl
+        .post("https://api.openai.com/v1/completions")
+        .bearer_auth(api_key)
         .header("Content-Type", "application/json")
-        .bearer_auth(api_key);
+        .body(serde_json::to_string(&request).unwrap())
+        .send()
+        .await;
+
+    console_log!("{:?}", result);
 }
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     Router::new()
-        .post_async("/summary", |req, env| async move {
-            let client = Client::new();
+        .post_async("/summary", |mut req, env| async move {
+            let data: BookData = req.json().await?;
+            openai_call(env.secret("OPENAI_KEY")?.to_string(), RequestType::Summarize, &data).await;
+
             Response::ok("lol")
         })
-        .post_async("/similar", |req, env| async move {
-            let client = Client::new();
-            Response::ok("xd")
+        .post_async("/similar", |mut req, env| async move {
+            let data: BookData = req.json().await?;
+            openai_call(env.secret("OPENAI_KEY")?.to_string(), RequestType::Similar, &data).await;
+
+            Response::ok("lol")
         })
         .run(req, env)
         .await
+        
 }
